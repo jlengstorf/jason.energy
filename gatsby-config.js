b0f22ac2bec14aaeac1384ce7d381795d370874e
Jason Lengstorf <jason@lengstorf.com>
2020-06-29 15:54:28 -0700

    wip: remove Google Fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-google-fonts',
-      options: {
-        fonts: ['Quicksand:wght@300,400,600'],
-        display: 'swap',
-      },
-    },
-    {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
