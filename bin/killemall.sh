1328fa2cad9d443df26cf0c151148ad55a4c727c
Jason Lengstorf <jason@lengstorf.com>
2020-08-20 15:06:37 -0700

    fix: no idea but maybe yes?

--- bin/killemall.sh
+++ bin/killemall.sh
@@ -1,2 +1,3 @@
-!#/bin/bash
-ps aux | grep postcss | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1 && exit 0
+#!/bin/bash
+ps aux | grep postcss | awk '{print $2}' | xargs kill -9 > /dev/null 2>&1
+exit 0
