c09a8db434308b9dac31e18907beb0ee2b15a2af
Jason Lengstorf <jason@lengstorf.com>
2020-08-20 15:44:45 -0700

    fix: hit it with a hammer

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,15 @@ module.exports = {
   plugins: [
     require('postcss-modules')({
       globalModulePaths: [/src\/styles\/global.css/],
-      getJSON:
-        // ctx.extractModules ||
-        (_filename, _json, _outputFile) => {},
+      // getJSON:
+      //   // ctx.extractModules ||
+      //   (_filename, _json, _outputFile) => {
+      //     console.log({
+      //       _filename,
+      //       _json,
+      //       _outputFile,
+      //     });
+      //   },
     }),
   ],
 };
--- src/components/block.js
+++ src/components/block.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import styles from '../styles/block.module.css';
+import { Helmet } from 'react-helmet';
+
+// import so PostCSS does its thing
+import _styles from '../styles/block.module.css';
+
+// this, because my life is a nightmare
+const styles = preval`
+  const fs = require('fs');
+  const path = require('path');
+  const parsedStylePath = path.resolve(__dirname, '../styles/block.module.css.json');
+  const styleJSON = fs.readFileSync(parsedStylePath, 'utf-8');
+
+  module.exports = JSON.parse(styleJSON);
+`;
 
 export function Block({ color, children, id, ...props }) {
   return (
-    <div id={id} className={`${styles.block} ${styles[color]}`} {...props}>
-      {children}
-    </div>
+    <>
+      <Helmet>
+        <link rel="stylesheet" href="/styles/block.module.css" />
+      </Helmet>
+      <div id={id} className={`${styles.block} ${styles[color]}`} {...props}>
+        {children}
+      </div>
+    </>
   );
 }
