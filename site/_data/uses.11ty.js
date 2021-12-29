b472087a3745f16950604f38698706f11072d8e3
Jason Lengstorf <jason@lengstorf.com>
2021-12-29 13:55:26 -0800

    wip: get uses function live and proxied

---
+++ site/_data/uses.11ty.js
@@ -0,0 +1,9 @@
+module.exports = async function () {
+  // const { default: fetch } = await import('node-fetch');
+  // console.log({ fetch });
+
+  // return await fetch(
+  //   `${process.env.URL || 'http://localhost:8888'}/api/uses`,
+  // ).then((res) => res.json());
+  return [];
+};
