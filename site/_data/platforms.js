48b558daa90d66524fc4cb25c8dce5bb6d7aab91
Jason Lengstorf <jason@lengstorf.com>
2022-01-09 22:30:13 -0800

    feat: more home page updates, cleanup

---
+++ site/_data/platforms.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    id: 'egghead',
+    label: 'egghead',
+    link: 'https://jason.af/egghead',
+    description: `
+      Jason has created dozens of video lessons on egghead. These videos skip
+      the chatter, instead diving straight into the code and delivering tons of
+      practical knowledge a few minutes at a time.
+    `,
+    image: 'egghead',
+  },
+  {
+    id: 'lwj',
+    label: 'Learn With Jason',
+    link: 'https://www.learnwithjason.dev',
+    description: `
+      Jason hosts <em>Learn With Jason</em> twice a week and pair programs with
+      brilliant people from community to learn something new in 90 minutes.
+      Shows are live and interactive, so 
+      <a href="https://www.learnwithjason.dev/schedule">mark your calendar</a> and join the fun!
+    `,
+    image: 'learnwithjason',
+  },
+  {
+    id: 'frontendmasters',
+    label: 'Frontend Masters',
+    link: 'https://jason.af/frontendmasters',
+    description: `
+      Jason has taught several workshops with Frontend Masters, a long-form
+      workshop format focused on deep dives into a topic for a full day. These
+      workshops are packed with the “why” & “how” behind the topic.
+    `,
+    image: 'frontendmasters',
+  },
+];
--- site/newsletter.md
+++ site/newsletter.md
@@ -7,16 +7,4 @@ description: >
   I share my best posts in my newsletter. I also occasionally share updates and news when it's relevant and helpful.
 ---
 
-<div class="opt-in">
-  <div class="opt-in-image">
-    <img loading="lazy" src="https://res.cloudinary.com/jlengstorf/image/upload/q_auto,f_auto,c_fill,w_280,h_296/jason.af/opt-in.jpg" alt="Illustration of a letter with a heart on the top flap." />
-  </div>
-  <form action="/api/subscribe" method="POST">
-    <label for="firstName">First Name</label>
-    <input id="firstName" name="firstName" type="text" />
-    <label for="email">Email</label>
-    <input id="email" name="email" type="email" />
-    <button type="submit">Subscribe</button>
-    <p class="notice"><small>I will never share your personal information because I'm not a jerk.</small></p>
-  </form>
-</div>
+{% include "shortcodes/newsletter-form.njk" %}
