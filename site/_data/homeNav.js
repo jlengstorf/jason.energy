5a4f03d9e1c42bdae5178d230076d9c10297a137
Jason Lengstorf <jason@lengstorf.com>
2022-01-13 22:20:40 -0800

    feat: add exploding nav

---
+++ site/_data/homeNav.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 'bio',
+    label: 'About Jason',
+    icon: 'camera',
+  },
+  {
+    id: 'teaching',
+    label: 'Teaching',
+    icon: 'terminal',
+  },
+  {
+    id: 'newsletter',
+    label: 'Newsletter',
+    icon: 'newsletter',
+  },
+  // {
+  //   id: 'connect',
+  //   label: 'Connect',
+  //   icon: 'rubber-corgi',
+  // },
+];
