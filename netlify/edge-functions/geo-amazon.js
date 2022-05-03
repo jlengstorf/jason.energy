6843e47a5f61d3113215e3577df9b791eb02dc98
Jason Lengstorf <jason@lengstorf.com>
2022-05-03 11:54:59 -0700

    fix: localize the right page

--- netlify/edge-functions/geo-amazon.js
+++ netlify/edge-functions/geo-amazon.js
@@ -11,14 +11,15 @@ export default async (_request, context) => {
   };
 
   const countryCode = context.geo?.country?.code || 'US';
-  const amazonTLD = AMAZON_TLDS[countryCode] || 'US';
+  // const amazonTLD = AMAZON_TLDS[countryCode] || 'US';
+  const amazonTLD = 'amazon.co.uk';
 
   console.log({ amazonTLD, countryCode });
 
   const response = await context.next();
   const text = await response.text();
 
-  response.headers.set('Content-Type', 'application/json');
+  response.headers.set('Content-Type', 'text/html');
 
   return new Response(text.replace(/amazon.com/gi, amazonTLD), response);
 };
