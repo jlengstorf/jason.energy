a95f8d46309f8450249423823dd509cf577537d0
Jason Lengstorf <jason@lengstorf.com>
2022-05-03 18:36:26 -0700

    feat: cleanup and full Amazon locale list

--- netlify/edge-functions/geo-amazon.js
+++ netlify/edge-functions/geo-amazon.js
@@ -1,35 +1,40 @@
 import { HTMLRewriter } from 'https://ghuc.cc/worker-tools/html-rewriter/index.ts';
 
+const AMAZON_LOCALES = {
+  AU: 'amazon.com.au',
+  BR: 'amazon.com.br',
+  CA: 'amazon.ca',
+  EG: 'amazon.eg',
+  FR: 'amazon.fr',
+  DE: 'amazon.de',
+  IN: 'amazon.in',
+  IT: 'amazon.it',
+  JP: 'amazon.co.jp',
+  MX: 'amazon.com.mx',
+  NL: 'amazon.nl',
+  PL: 'amazon.pl',
+  SG: 'amazon.sg',
+  SA: 'amazon.sa',
+  ES: 'amazon.es',
+  SE: 'amazon.se',
+  TR: 'amazon.com.tr',
+  AE: 'amazon.ae',
+  GB: 'amazon.co.uk',
+  US: 'amazon.com',
+};
+
 export default async (_request, context) => {
-  console.log('Waddup from an Edge Function!');
-
-  const AMAZON_TLDS = {
-    US: 'amazon.com',
-    UK: 'amazon.co.uk',
-    CA: 'amazon.ca',
-    JP: 'amazon.co.jp',
-    DE: 'amazon.de',
-    IN: 'amazon.in',
-  };
-
   const countryCode = context.geo?.country?.code || 'US';
-  const amazonTLD = AMAZON_TLDS[countryCode] || 'US';
-
-  console.log({ amazonTLD, countryCode });
+  const amazonTLD = AMAZON_LOCALES[countryCode] || 'amazon.com';
 
   const response = await context.next();
-
-  response.headers.set('X-Booped', 'yup');
 
   return new HTMLRewriter()
     .on('a', {
       element(element) {
         const href = element.getAttribute('href');
         if (href.includes('amazon.com')) {
-          element.setAttribute(
-            'href',
-            href.replace('amazon.com', 'amazon.co.uk'),
-          );
+          element.setAttribute('href', href.replace('amazon.com', amazonTLD));
         }
       },
     })
