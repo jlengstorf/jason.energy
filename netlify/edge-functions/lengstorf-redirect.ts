797dccbadf9f37868f23d419bf5d67ba4dad82fc
Jason Lengstorf <jason@lengstorf.com>
2022-08-21 16:53:56 -0700

    feat: show notice on redirects from lengstorf.com

---
+++ netlify/edge-functions/lengstorf-redirect.ts
@@ -0,0 +1,43 @@
+import { Context } from 'https://edge.netlify.com';
+import { HTMLRewriter } from 'https://ghuc.cc/worker-tools/html-rewriter/index.ts';
+
+export default async (request: Request, context: Context) => {
+  console.log(request.headers.entries());
+
+  if (
+    !request.headers.has('X-From') ||
+    request.headers.get('X-From') !== 'lengstorf.com'
+  ) {
+    return;
+  }
+
+  /*
+   * If we’re here, it’s because this request is a redirect from lengstorf.com,
+   * likely due to visiting a page that I’ve taken down from that site.
+   *
+   * To make the redirect less jarring, let’s add a note letting people know
+   * that they were trying to read a really old post that I wouldn’t recommend
+   * anymore.
+   */
+  const response = await context.next();
+
+  return new HTMLRewriter()
+    .on('.heading', {
+      element(element) {
+        element.prepend(
+          `
+            <div class="post-aside" style="margin-bottom: 3rem;">
+              <div class="post-aside-icon">
+                <img src="https://res.cloudinary.com/jlengstorf/image/upload/w_80,f_auto,q_auto/v1593991250/jason.af/light-mode.png" alt="light bulb" style="width: 100%;">
+              </div>
+              <div class="post-aside-content" style="text-align: left;">
+                <p>Hi, friend! You’ve been redirected here because you followed a link to a post that is <em>very</em> old and that I don’t consider relevant anymore. You can find my more recent writing below!</p>
+              </div>
+            </div>
+          `,
+          { html: true },
+        );
+      },
+    })
+    .transform(response);
+};
