705ff882c509cd4d1d4b76ec33a84fa39db3a5a8
Jason Lengstorf <jason@lengstorf.com>
2022-08-21 17:47:53 -0700

    feat: alright, fine, we do it this way

--- netlify/edge-functions/lengstorf-redirect.ts
+++ netlify/edge-functions/lengstorf-redirect.ts
@@ -1,13 +1,9 @@
-import { Context } from 'https://edge.netlify.com';
 import { HTMLRewriter } from 'https://ghuc.cc/worker-tools/html-rewriter/index.ts';
 
-export default async (request: Request, context: Context) => {
+export default async (request: Request) => {
   const url = new URL(request.url);
 
-  if (
-    !url.searchParams.has('from') ||
-    url.searchParams.get('from') !== 'lengstorf.com'
-  ) {
+  if (url.pathname !== '/posts/redirected') {
     return;
   }
 
@@ -19,7 +15,8 @@ export default async (request: Request, context: Context) => {
    * that they were trying to read a really old post that I wouldn’t recommend
    * anymore.
    */
-  const response = await context.next();
+  url.pathname = '/posts';
+  const response = await fetch(url.toString());
 
   return new HTMLRewriter()
     .on('.heading', {
@@ -28,10 +25,18 @@ export default async (request: Request, context: Context) => {
           `
             <div class="post-aside" style="margin-bottom: 3rem;">
               <div class="post-aside-icon">
-                <img src="https://res.cloudinary.com/jlengstorf/image/upload/w_80,f_auto,q_auto/v1593991250/jason.af/light-mode.png" alt="light bulb" style="width: 100%;">
+                <img
+                  src="https://res.cloudinary.com/jlengstorf/image/upload/w_80,f_auto,q_auto/v1593991250/jason.af/light-mode.png"
+                  alt="light bulb"
+                  style="width: 100%;"
+                />
               </div>
               <div class="post-aside-content" style="text-align: left;">
-                <p>Hi, friend! You’ve been redirected here because you followed a link to a post that is <em>very</em> old and that I don’t consider relevant anymore. You can find my more recent writing below!</p>
+                <p>
+                  Hi, friend! You’ve been redirected here because you followed 
+                  a link to a post that is outdated and that I don’t consider 
+                  relevant anymore. You can find my more recent writing below!
+                </p>
               </div>
             </div>
           `,
